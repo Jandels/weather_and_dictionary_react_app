3ce7bb19752b91e1cf10a514e9bfd5970c9cc64f
Jandels <ej.humphrey96@gmail.com>
2021-11-04 12:30:49 +0000

    Added Boostrap to Project

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <body className="App-header">
-        <h1>
-          Welcome <br />
-        </h1>
+      <div className="container">
+        <body className="App-header">
+          <h1>
+            Welcome <br />
+          </h1>
 
-        <h3>Emma's Dictionary and Weather App Portfollio</h3>
-      </body>
+          <h3>Emma's Dictionary and Weather App Portfollio</h3>
+        </body>
 
-      <footer>
-        This project was coded by Jandels and is{" "}
-        <a
-          href="https://github.com/Jandels/weather_and_dictionary_react_app"
-          target="_blank"
-        >
-          open sourced
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Jandels and is{" "}
+          <a
+            href="https://github.com/Jandels/weather_and_dictionary_react_app"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
