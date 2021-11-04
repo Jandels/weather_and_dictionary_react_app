8e354c2382d5fc755500b891375a14fcb8a56740
Jandels <ej.humphrey96@gmail.com>
2021-11-04 12:18:14 +0000

    Added Open Source link to bottom of page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <body className="App-header">
         <h1>
           Welcome <br />
         </h1>
+
         <h3>Emma's Dictionary and Weather App Portfollio</h3>
-      </header>
+      </body>
+
+      <footer>
+        This project was coded by Jandels and is{" "}
+        <a
+          href="https://github.com/Jandels/weather_and_dictionary_react_app"
+          target="_blank"
+        >
+          open sourced
+        </a>
+      </footer>
     </div>
   );
 }
