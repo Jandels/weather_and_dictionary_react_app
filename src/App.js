1fbef1c50bd7eb862a9385fac5c66fc35dafde7f
Jandels <ej.humphrey96@gmail.com>
2021-11-04 12:10:49 +0000

    Imported and Cleaned up React folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Welcome <br />
+        </h1>
+        <h3>Emma's Dictionary and Weather App Portfollio</h3>
       </header>
     </div>
   );
 }
-
-export default App;
