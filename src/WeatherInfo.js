4536b36aa3a616716829edc2a5e46e0f4563a179
Jandels <ej.humphrey96@gmail.com>
2021-11-14 19:50:36 +0000

    Name Change 5

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,7 +6,7 @@ export default function WeatherInfo(props) {
     <div className="WeatherInfo">
       <div className="row">
         <div className="col-5 current-weather">
-          <h1>{props.data.city}</h1>
+          <h1>Weather in {props.data.city}</h1>
 
           <div className="row">
             <div className="col-6">
