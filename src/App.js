7de31df681c4fb754d232eb2fde285d76e4af15f
Jandels <ej.humphrey96@gmail.com>
2021-11-14 19:33:26 +0000

    Added Netlify Link to prevent future deploy details

--- src/App.js
+++ src/App.js
@@ -8,14 +8,23 @@ export default function App() {
         {/* Reminder ^ This code is used for Bootstrap */}
         <WeatherApp defaultcity="Wellington" />
         <p className="footer">
-          This project was coded by Jandels and is{" "}
+          This project was coded by Jandels and is open sourced on{" "}
           <a
             href="https://github.com/Jandels/weather_and_dictionary_react_app"
             target="_blank"
             rel="noreferrer"
           >
-            open sourced on Github
-          </a>
+            {" "}
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://github.com/Jandels/weather_and_dictionary_react_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>{" "}
         </p>
       </div>
     </div>
